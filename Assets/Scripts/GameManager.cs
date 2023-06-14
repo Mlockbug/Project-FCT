f85f0c8869ad7a1f3cd3181f41a97b8dfaa062e2
Mlockbug <energetichamster@gmail.com>
2023-06-14 12:43:39 +0100

    v0.1.1b
    start of showing new pieces, update to bag

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,29 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-	public GameObject[] tetrominoes, tempTetrominoes;
+	public GameObject[] tetrominoes;
+	public Transform[] nextPieceSpawns;
+	GameObject[] tempTetrominoes;
 	Rigidbody2D currentPiece;
-	GameObject tempPiece;
-	Queue<GameObject> bag, nextPieces;
-	bool mustDrop;
+	GameObject tempPiece, r_tempBlock;
+	Queue<GameObject> bag = new Queue<GameObject>();
+	GameObject[] nextPieces;
+	bool mustDrop, refillBag;
 	float gravityWait;
 	int count;
 	void Start() {
-
+		FillBag(true);
 	}
 
 	void Update() {
-		if (bag == null) {
-			tempTetrominoes = tetrominoes;
-			while (count  < 7) {
-				int randomTemp = Random.Range(0, 7);
-				if (tempTetrominoes[randomTemp] != null) {
-					bag.Enqueue(tempTetrominoes[randomTemp]);
-					tempTetrominoes[randomTemp] = null;
-					count++;
-				}
-			}
-			count = 0;
+		if (bag.Count == 0) {
+			FillBag(false);
 		}
 		if (currentPiece == null) {
 			SpawnNewPiece();
@@ -42,9 +36,33 @@ public class GameManager : MonoBehaviour {
 		}
 		StopCoroutine(Gravity());
 	}
-
+	void FillBag(bool start) {
+		tempTetrominoes = tetrominoes;
+		while (count < 7) {
+			int randomTemp = Random.Range(0, 7);
+			if (tempTetrominoes[randomTemp] != null) {
+				bag.Enqueue(tempTetrominoes[randomTemp]);
+				tempTetrominoes[randomTemp] = null;
+				count++;
+			}
+		}
+		count = 0;
+		if (start) {
+			for (int i = 0; i< 5; i++) {
+				nextPieces[i] = bag.Dequeue();
+			}
+		}
+	}
 	void SpawnNewPiece() {
-		tempPiece = bag.Dequeue();
-		nextPieces.Dequeue();
+		
+	}
+
+	void RefreshNextPieces() {
+		for (int i = 0; i<5; i++) {
+			r_tempBlock = Instantiate(nextPieces[i], nextPieceSpawns[i].position, nextPieceSpawns[i].rotation);
+			if (r_tempBlock.name.Contains("O-") || r_tempBlock.name.Contains("I-")){
+				r_tempBlock.transform.position += new Vector3(0, 0.5f, 0);
+			}
+		}
 	}
 }
